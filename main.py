77672b65ba79d9a17cc5148a8e78c43c12bf9a9b
Zoltan <zzz@adresse.fr>
2013-11-28 15:45:32 +0100

    Join a hub

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tripForm
 		
 app = webapp2.WSGIApplication([('/', hub.HubHandler),
 								('/_create', tripForm.TripFormHandler),
+								('/_join', tripForm.JoinHubHandler),
 								('/(\d+)', trip.TripHandler)
 								]
 								, debug=True)
--- tripForm.py
+++ tripForm.py
@@ -11,12 +11,16 @@ import handler
 
 USER_NAME_REGEX = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 TRIP_NAME_REGEX = re.compile(r"^[a-zA-Z0-9][a-zA-Z0-9_\s]{3,200}$")
+HUB_ID_REGEX = re.compile(r"^[0-9]{1,500}$")
 
 def valid_user_name(user_name):
 	return USER_NAME_REGEX.match(user_name)
 
 def valid_trip_name(trip_name):
 	return TRIP_NAME_REGEX.match(trip_name)
+	
+def valid_hub_id(hub_id):
+	return HUB_ID_REGEX.match(hub_id)
 	
 def escape_html(s):
 	return cgi.escape(s, quote = True)
@@ -53,3 +57,23 @@ class TripFormHandler(handler.Handler):
 			e = database.Discussion(title=trip_name,comments=[],sondages=[])
 			e.put()
 			self.redirect("/"+str(e.key().id()))
+			
+class JoinHubHandler(handler.Handler):
+	def write_form(self, hubID="", error_hubID=""):
+		self.render("joinHubForm.html",
+		hub_ID=hubID,
+		error_hubID=error_hubID) 
+
+	def get(self):
+		self.write_form();
+		
+	def post(self):
+		hub_ID = self.request.get('hubID')
+		hubID = valid_hub_id(str(hub_ID))
+		error_hubID=""
+        
+		if not hubID:
+			error_hubID = "The hub ID is numerical."
+			self.write_form(str(hub_ID), error_hubID)
+		else:
+			self.redirect("/"+str(hub_ID))
