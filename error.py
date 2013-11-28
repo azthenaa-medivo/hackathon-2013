526eedafe00f8a93145fe0df8715389bb3b1d5ce
Zoltan <zzz@adresse.fr>
2013-11-28 16:14:24 +0100

    Page d'erreur

---
+++ error.py
@@ -0,0 +1,13 @@
+﻿import re
+import cgi
+import string
+
+def escape_html(s):
+	return cgi.escape(s, quote = True)
+	
+class ErrorHandler(handler.Handler):
+	def write_form(self):
+		self.render("error.html")
+
+	def get(self):
+		self.write_form();
