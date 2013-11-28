e7d00ee0157c6e435a49f79c1345d07046a7b81d
Julien <julienbastian@yahoo.fr>
2013-11-28 21:05:29 +0100

    Send an email to the creator of the trip.

--- tripForm.py
+++ tripForm.py
@@ -1,15 +1,19 @@
+from google.appengine.api import mail
 from google.appengine.ext import db
 
 import database
 import handler
 import security
+import emailUtility
 
 class TripFormHandler(handler.Handler):
-	def write_form(self, user_name="", trip_name="", error_username="", error_tripname=""):
+
+	def write_form(self, user_name="", trip_name="", error_username="", error_email="", error_tripname=""):
 		self.render("tripForm.html",
 		user_name=user_name,
 		trip_name=trip_name,
 		error_username=error_username,
+		error_email=error_email,
 		error_tripname=error_tripname) 
 
 	def get(self):
@@ -18,21 +22,27 @@ class TripFormHandler(handler.Handler):
 	def post(self):
 		user_name = self.request.get('username')
 		trip_name = self.request.get('tripname')
+		email = self.request.get('email')
 
 		username = security.valid_user_name(user_name) 
 		tripname = security.valid_trip_name(trip_name)
 
 		error_username=""
 		error_tripname=""
+		error_email=""
         
-		if not username :
+		if not username:
 			error_username = "That's not a valid username."
 		if not tripname :
-			error_tripname = "That wasn't a valid trip name."
-
-		if not (username and tripname):
-			self.write_form( security.escape_html(user_name), security.escape_html(trip_name), error_username, error_tripname)
+			error_tripname = "That's not a valid trip name."
+		if not mail.is_email_valid(email):
+			error_email = "That's not a valid email."
+			
+		if error_username or error_email or error_tripname:
+			self.write_form( security.escape_html(user_name), security.escape_html(trip_name), error_username, error_email, error_tripname)
 		else:
 			e = database.Discussion(title=trip_name)
 			e.put()
-			self.redirect("/"+str(e.key().id()))
+			trip_id = str(e.key().id())
+			emailUtility.send_email(user_name,trip_name,email,trip_id)
+			self.redirect("/"+trip_id)
