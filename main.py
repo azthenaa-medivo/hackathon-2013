4f0952f85f5f55dcec48bad4ac25455ba7532480
Alexis <fouche.alexis@gmail.com>
2013-11-28 20:13:07 +0100

    places : first implementation

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import trip
 import tripForm
 import error
 import joinHubForm
+import places
 		
 app = webapp2.WSGIApplication([('/', hub.HubHandler),
 								('/_create', tripForm.TripFormHandler),
 								('/_join', joinHubForm.JoinHubHandler),
+								('/_places', places.placesHandler),
 								('/(\d+)', trip.TripHandler),
 								('/.*', error.ErrorHandler)
 								]
