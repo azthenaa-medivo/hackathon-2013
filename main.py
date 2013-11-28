64db2cd936cd4e622ccfa31acfc10c8377b7c791
Julien <julienbastian@yahoo.fr>
2013-11-28 11:47:22 +0100

    Add a basic trip form

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import webapp2
         
-import hub 		
+import hub
+import trip	
 
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 		
 app = webapp2.WSGIApplication([('/', hub.HubHandler),
+								('/_create', trip.TripHandler),
 								(PAGE_RE, hub.HubHandler)
 								]
 								, debug=True)
