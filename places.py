a8cd91c18f96d9e321e504784cd66a8c49c15e78
Alexis <fouche.alexis@gmail.com>
2013-11-29 12:10:12 +0100

    places implementation

--- places.py
+++ places.py
@@ -12,55 +12,98 @@ class placesHandler(handler.Handler):
 	url_places = "http://api.outpost.travel/placeRentals"
 
 	def get(self):
-		self.render_all()
+		if self.request.get('id'):
+			self.render_id(self.request.get('id'))
+		elif self.request.get('city'):
+			self.render_city(self.request.get('city'))
+		elif self.request.get('sort'):
+			self.render_sort(self.request.get('sort'))
+		else:
+			self.render_all()
+			
+
 
 	def render_all(self):
 		plain = self.process(self.url_places)
 		jSon = json.loads(plain)
-		places_list = self.jSonToList(jSon)
-		self.render("places.html", places_list=places_list, page=jSon["page"], total_pages=jSon["totalPages"], total_results=jSon["totalResults"])
+		places_list = []
+		for item in jSon["items"]:
+			places_list.append(self.jSonToPlace(item))
+		self.render("places_list.html", places_list=places_list, page=jSon["page"], total_pages=jSon["totalPages"], total_results=jSon["totalResults"])
+
+	def render_city(self, city):
+		url = self.url_places + "?city=" + city
+		plain = self.process(self.url_places)
+		jSon = json.loads(plain)
+		places_list = []
+		for item in jSon["items"]:
+			places_list.append(self.jSonToPlace(item))
+		self.render("places_list.html", city=city, places_list=places_list, page=jSon["page"], total_pages=jSon["totalPages"], total_results=jSon["totalResults"])
+
+	def render_sort(self, sort):
+		if self.request.get('sortMethod'):
+			sortMethod = self.request.get('sortMethod')
+		else:
+			sortMethod = 'ascending'
+		url = self.url_places + "?sort=" + sort + "?sortMethod=" + sortMethod
+		plain = self.process(self.url_places)
+		jSon = json.loads(plain)
+		places_list = []
+		for item in jSon["items"]:
+			places_list.append(self.jSonToPlace(item))
+		self.render("places_list.html", sort=sort, places_list=places_list, page=jSon["page"], total_pages=jSon["totalPages"], total_results=jSon["totalResults"])
+
+
+	def render_id(self, place_id):
+		url = self.url_places + "?pid=" + place_id
+		plain = self.process(url)
+		jSon = json.loads(plain)
+		if len(jSon["items"]) > 0:
+			self.render("place.html", place=self.jSonToPlace(jSon["items"][0]))
+		else:
+			self.render("place.html", error="Cet ID est introuvable")
 
 	def process(self, url):
 		req = urllib2.urlopen(url)
 		return req.read()
 
-	def jSonToList(self, jSon):
-		res = []
-		for item in jSon["items"]:
-			place = places()
-			place.origin = item["origin"]
-			place.minimum_stay_night = item["minimumStayNight"]
-			place.price_per = item["pricePer"]
-			place.bed_count = item["bedCount"]
-			place.occupancy = item["occupancy"]
-			place.provider = item["provider"]
-			place.description = item["description"]
-			place.pid = item["pid"]
-			place.price = item["price"]
-			place.link = item["link"]
-			place.country = item["country"]
-			place.nid = item["nid"]
-			place.bathroomcount = item["bathroomCount"]
-			place.bedroomcount = item["bedroomCount"]
-			place.heading = item["heading"]
-			place.room_type = item["type"]["roomType"]
-			place.room_type_alias = item["typeAlias"]["roomType"]
-			place.prop_type = item["type"]["propType"]
-			place.prop_type_alias = item["typeAlias"]["propType"]
-			place.latitude = item["latLng"][0]
-			place.longitude = item["latLng"][1]
-			for amenity in item["amenities"]:
-				place.amenities.append(amenity)
-			for unavlbl in item["unavailable"]:
-				place.unavailable.append(unavlbl)
-			for photo in item["photos"]:
-				place.photos.append(photo["url"])
-			res.append(place)
-		return res
+	def jSonToPlace(self, item):
+		myPlace = place()
+		myPlace.photos = []
+		myPlace.amenities = []
+		myPlace.unavailable = []
+		myPlace.origin = item["origin"]
+		myPlace.minimum_stay_night = item["minimumStayNight"]
+		myPlace.price_per = item["pricePer"]
+		myPlace.bed_count = item["bedCount"]
+		myPlace.occupancy = item["occupancy"]
+		myPlace.provider = item["provider"]
+		myPlace.description = item["description"]
+		myPlace.pid = item["pid"]
+		myPlace.price = item["price"]
+		myPlace.link = item["link"]
+		myPlace.country = item["country"]
+		myPlace.nid = item["nid"]
+		myPlace.bathroomcount = item["bathroomCount"]
+		myPlace.bedroomcount = item["bedroomCount"]
+		myPlace.heading = item["heading"]
+		myPlace.room_type = item["type"]["roomType"]
+		myPlace.room_type_alias = item["typeAlias"]["roomType"]
+		myPlace.prop_type = item["type"]["propType"]
+		myPlace.prop_type_alias = item["typeAlias"]["propType"]
+		myPlace.latitude = item["latLng"][0]
+		myPlace.longitude = item["latLng"][1]
+		for amenity in item["amenities"]:
+			myPlace.amenities.append(amenity)
+		for unavlbl in item["unavailable"]:
+			myPlace.unavailable.append(unavlbl)
+		for photo in item["photos"]:
+			myPlace.photos.append(photo["url"])
+		return myPlace
 
 
 
-class places:
+class place:
 	origin=""
 	minimum_stay_night=""
 	price_per=""
